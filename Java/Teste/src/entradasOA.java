c48f70e737800bade72ad60d76f8ded8f1205647
erickjuan50 <erickjuan50@gmail.com>
2023-02-13 02:14:26 -0300

    Teste
    Primeiro teste de Commit

---
+++ Java/Teste/src/entradasOA.java
@@ -0,0 +1,9 @@
+public class entradasOA {
+    public static void main(String[] args){
+        operacoes_aritmeticas entradasOA = new operacoes_aritmeticas();
+        entradasOA.numero1 = 1;
+        entradasOA.numero2 = 3;
+        entradasOA.soma();
+    
+    }
+}
---
+++ Java/Teste/src/operacoes_aritmeticas.java
@@ -0,0 +1,8 @@
+public class operacoes_aritmeticas {
+    public int numero1;
+    public int numero2;
+public void soma(){
+    System.out.println(numero1+numero2);
+}
+}
+
